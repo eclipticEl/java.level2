048c074d7d704d4aee175a8a2a9f54837bf77341
Elena Zhidko <hpivnenko@gmail.com>
2019-11-11 22:58:10 +0300

    выполнено задание к 4-му уроку

---
+++ src/lesson4/ChatServer.java
@@ -0,0 +1,11 @@
+package lesson4;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.printf("Server starts at port: %d\n", port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
---
+++ src/lesson4/ClientGUI.java
@@ -0,0 +1,123 @@
+package lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top", true);
+    private final JTextField tfLogin = new JTextField("ivan");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    private final JList<String> userList = new JList<>();
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
+
+    private ClientGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        setTitle("Chat Client");
+        setAlwaysOnTop(true);
+        JScrollPane scrollLog = new JScrollPane(log);
+        JScrollPane scrollUsers = new JScrollPane(userList);
+        String[] users = {"user1", "user2", "user3", "user4",
+                "user5", "user6", "user7", "user8", "a very_long_named_user_in_this_chat"};
+        userList.setListData(users);
+        scrollUsers.setPreferredSize(new Dimension(100, 0));
+
+        cbAlwaysOnTop.addActionListener(this);
+        btnSend.addActionListener(this);
+        tfMessage.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_ENTER) {
+                    addToLog();
+                }
+            }
+        });
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        add(scrollLog, BorderLayout.CENTER);
+        add(scrollUsers, BorderLayout.EAST);
+        add(panelBottom, BorderLayout.SOUTH);
+        add(panelTop, BorderLayout.NORTH);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+        } else if (src == btnSend){
+            addToLog();
+        }
+        else {
+            throw new RuntimeException("Unknown source: " + src);
+        }
+    }
+
+    private void addToLog() {
+
+        log.append(tfLogin.getText() + "\t" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))+ "\n");
+        log.append(tfMessage.getText()+"\n");
+        tfMessage.setText("");
+
+        try (FileWriter fout = new FileWriter("MessageLog.txt")){
+            log.write(fout);
+        }catch (IOException e){
+            JOptionPane.showMessageDialog(this, "Ошибка записи в файл истории сообщений", "Exception", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        msg = e.getClass().getCanonicalName() + ": " +
+                e.getMessage() + "\n\t" + ste[0];
+
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
+
+}
